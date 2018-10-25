097d3bc34a4efa110e88004feaeb1f04a032541d
Pig Fang <g-plane@hotmail.com>
2018-10-25 10:24:32 +0800

    add tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,21 +1,47 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert'
+import * as http from 'http'
+import * as vscode from 'vscode'
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+describe('Commands', () => {
+  it('play previous song', done => {
+    const server = http
+      .createServer((req, res) => {
+        res.end()
+        if (req.method === 'POST') {
+          assert.strictEqual(req.url, '/api/player/previous')
+        }
+        server.close()
+      })
+      .listen(8880)
+      .on('close', done)
+    vscode.commands.executeCommand('beefweb.playPrev')
+  })
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite('Extension Tests', () => {
-  // Defines a Mocha unit test
-  test('Something 1', () => {
-    assert.equal(-1, [1, 2, 3].indexOf(5))
-    assert.equal(-1, [1, 2, 3].indexOf(0))
+  it('play next song', done => {
+    const server = http
+      .createServer((req, res) => {
+        res.end()
+        if (req.method === 'POST') {
+          assert.strictEqual(req.url, '/api/player/next')
+        }
+        server.close()
+      })
+      .listen(8880)
+      .on('close', done)
+    vscode.commands.executeCommand('beefweb.playNext')
+  })
+
+  it('toggle pause', done => {
+    const server = http
+      .createServer((req, res) => {
+        res.end()
+        if (req.method === 'POST') {
+          assert.strictEqual(req.url, '/api/player/pause/toggle')
+        }
+        server.close()
+      })
+      .listen(8880)
+      .on('close', done)
+    vscode.commands.executeCommand('beefweb.togglePause')
   })
 })
